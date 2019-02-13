c1a4ae4bbf3e65e3a4e31944189a01490a7e4e02
Laura Royle <lauraamie.royle@gmail.com>
2019-02-13 10:44:54 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,6 +13,12 @@ def first_challenge
     }
   }
 
-  contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
-  contacts
+contacts["Freddy Mercury"].each do |keys, values| #attribute is the keys (name, email etc) & data is the values
+    if keys == :favorite_icecream_flavors
+      values.delete_if do |flavour|
+        flavour == "strawberry"
+        end
+      end
+   end
+   contacts
 end
